15035cafe53f5f1ae05ac6522ade3dbd6a8b8553
DESKTOP-FGH1MTQ\Marcin <owik100@gmail.com>
2018-12-08 13:06:41 +0100

    Chenged colors, added english version, changed script on load

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
-window.onload=function()
-{
-    $('a').click(function(){
-        $('html, body').animate({
-            scrollTop: $( $(this).attr('href') ).offset().top
-        }, 500);
-        return false;
-    });
-}
+$("document").ready(
+    function()
+    {
+        $('a').click(function(){
+            $('html, body').animate({
+                scrollTop: $( $(this).attr('href') ).offset().top
+            }, 500);
+            return false;
+        });
+    }
+)
